e3321735e51d2d8a74b763ae1368b4a978938278
gboeing <gboeing@berkeley.edu>
2016-12-23 18:28:20 -0800

    added coverage test

--- tests/test_osmnx.py
+++ tests/test_osmnx.py
@@ -4,13 +4,18 @@ OSMnx tests
 """
 
 import matplotlib as mpl
-mpl.use('Agg') #use agg backend so you don't need a display on travis
+mpl.use('Agg') #use agg backend so you don't need a display on travis-ci
 
-import osmnx as ox
+import osmnx as ox, logging as lg
 ox.config(log_console=True, log_file=True, use_cache=True, 
           data_folder='.temp/data', logs_folder='.temp/logs', imgs_folder='.temp/imgs', cache_folder='.temp/cache')
 
-          
+ox.log('test debug', level=lg.DEBUG)
+ox.log('test info', level=lg.INFO)
+ox.log('test warning', level=lg.WARNING)
+ox.log('test error', level=lg.ERROR)
+
+
 def test_imports():
     import json, math, sys, os, io, ast, unicodedata, hashlib, re, random, time, datetime as dt, logging as lg
     from collections import OrderedDict, Counter
@@ -32,6 +37,9 @@ def test_gdf_shapefiles():
     city_projected = ox.project_gdf(city)
     ox.save_gdf_shapefile(city_projected)
     
+    city = ox.gdf_from_place('Manhattan, New York City, New York, USA', buffer_dist=100)
+    ox.plot_shape(city)
+    
     
 def test_network_saving_loading():
     G = ox.graph_from_place('Piedmont, California, USA')
@@ -46,8 +54,10 @@ def test_get_network_methods():
     import geopandas as gpd
     north, south, east, west = 37.79, 37.78, -122.41, -122.43
     G1 = ox.graph_from_bbox(north, south, east, west, network_type='drive_service')
+    G1 = ox.graph_from_bbox(north, south, east, west, network_type='drive_service', truncate_by_edge=True)
 
     location_point = (37.791427, -122.410018)
+    bbox = ox.bbox_from_point(location_point, project_utm=True)
     G2 = ox.graph_from_point(location_point, distance=750, distance_type='bbox', network_type='drive')
     G3 = ox.graph_from_point(location_point, distance=500, distance_type='network')
 
@@ -66,16 +76,19 @@ def test_stats():
     location_point = (37.791427, -122.410018)
     G = ox.graph_from_point(location_point, distance=500, distance_type='network')
     stats1 = ox.basic_stats(G)
+    stats1 = ox.basic_stats(G, area=1000)
     stats2 = ox.extended_stats(G, connectivity=True, anc=True, ecc=True, bc=True, cc=True)
     
     
 def test_plots():
     
     G = ox.graph_from_place('Piedmont, California, USA', network_type='drive', simplify=False)
+    G2 = ox.simplify_graph(G, strict=False)
+    ec = ox.get_edge_colors_by_attr(G2, 'length')
     fig, ax = ox.plot_graph(G, save=True, file_format='png')
-
+    
     G_simplified = ox.simplify_graph(G)
-    fig, ax = ox.plot_graph(G_simplified, show=False, save=True, close=True, file_format='png')
+    fig, ax = ox.plot_graph(G_simplified, show=False, save=True, close=True, file_format='svg')
 
     G_projected = ox.project_graph(G_simplified)
     fig, ax = ox.plot_graph(G_projected)
@@ -95,5 +108,7 @@ def test_routing():
     destination_node = ox.get_nearest_node(G, destination)
     route = nx.shortest_path(G, origin_node, destination_node)
     fig, ax = ox.plot_graph_route(G, route, save=True, filename='route', file_format='png')
+    fig, ax = ox.plot_graph_route(G, route, origin_point=origin, destination_point=destination,
+                                  save=True, filename='route', file_format='png')
     
     
