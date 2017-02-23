348cd932115866d45b7ec8075e21ad305176de7d
gboeing <gboeing@berkeley.edu>
2017-02-22 17:10:46 -0800

    replace geopy geocoder with new function in utils

--- osmnx/utils.py
+++ osmnx/utils.py
@@ -313,4 +313,35 @@ def get_nearest_node(G, point, return_dist=False):
     else:
         return nearest_node
         
+
+def geocode(query):
+    """
+    Geocode a query string to (lat, lon) with the Nominatim geocoder
+
+    Parameters
+    ----------
+    query : string
+        the query string to geocode
+
+    Returns
+    -------
+    point : tuple
+        the (lat, lon) coordinates returned by the geocoder
+    """
+    
+    # send the query to the nominatim geocoder and parse the json response
+    url_template = 'https://nominatim.openstreetmap.org/search?format=json&limit=1&q={}'
+    url = url_template.format(query)
+    response = requests.get(url, timeout=60)
+    results = response.json()
+    
+    # if results were returned, parse lat and long out of the result
+    if len(results) > 0 and 'lat' in results[0] and 'lon' in results[0]:
+        lat = float(result['lat'])
+        lon = float(result['lon'])
+        point = (lat, lon)
+        return point
+    else:
+        raise Exception('Nominatim geocoder returned no results for query "{}"'.format(query))
+
         
