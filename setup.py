e68ddacd613f8bfcaca90fcd4b99d839abc9d0af
gboeing <gboeing@berkeley.edu>
2017-05-12 16:35:17 -0700

    version bump

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ classifiers=['Development Status :: 5 - Production/Stable',
 
 # now call setup
 setup(name='osmnx',
-      version='0.5',
+      version='0.5.1',
       description='Retrieve, construct, analyze, and visualize street networks from OpenStreetMap',
       long_description=long_description,
       classifiers=classifiers,
