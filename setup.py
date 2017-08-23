f9177d3b1fca41545fe3ccb59fc087a4fc4c8991
gboeing <gboeing@berkeley.edu>
2017-08-23 14:06:11 -0700

    update requirements

--- setup.py
+++ setup.py
@@ -5,12 +5,13 @@ from setuptools import setup
 
 # provide a long description using reStructuredText
 long_description = """
-**OSMnx** is a package to easily download, construct, project, visualize, and analyze complex street
-networks from OpenStreetMap in Python with NetworkX.
+**OSMnx** is a package to easily download, construct, project, visualize, and
+analyze complex street networks from OpenStreetMap in Python with NetworkX.
 
-You can get a city's or neighborhood's walking, driving, or biking network with a single line of Python
-code. Then you can easily visualize cul-de-sacs or one-way streets, plot shortest-path routes, or
-calculate stats like intersection density, average node connectivity, betweenness centrality, etc.
+You can get a city's or neighborhood's walking, driving, or biking network with
+a single line of Python code. Then you can easily visualize cul-de-sacs or
+one-way streets, plot shortest-path routes, or calculate stats like intersection
+density, average node connectivity, betweenness centrality, etc.
 
 See the examples and demos on `GitHub`_ or read more about `OSMnx`_.
 
@@ -19,25 +20,25 @@ See the examples and demos on `GitHub`_ or read more about `OSMnx`_.
 """
 
 # list of classifiers from the PyPI classifiers trove
-classifiers=['Development Status :: 5 - Production/Stable',
-             'License :: OSI Approved :: MIT License',
-             'Operating System :: OS Independent',
-             'Intended Audience :: Science/Research',
-             'Topic :: Scientific/Engineering :: GIS',
-             'Topic :: Scientific/Engineering :: Visualization',
-             'Topic :: Scientific/Engineering :: Physics',
-             'Topic :: Scientific/Engineering :: Information Analysis',
-             'Programming Language :: Python',
-             'Programming Language :: Python :: 2',
-             'Programming Language :: Python :: 3',
-             'Programming Language :: Python :: 2.7',
-             'Programming Language :: Python :: 3.4',
-             'Programming Language :: Python :: 3.5',
-             'Programming Language :: Python :: 3.6']
+classifiers = ['Development Status :: 5 - Production/Stable',
+               'License :: OSI Approved :: MIT License',
+               'Operating System :: OS Independent',
+               'Intended Audience :: Science/Research',
+               'Topic :: Scientific/Engineering :: GIS',
+               'Topic :: Scientific/Engineering :: Visualization',
+               'Topic :: Scientific/Engineering :: Physics',
+               'Topic :: Scientific/Engineering :: Information Analysis',
+               'Programming Language :: Python',
+               'Programming Language :: Python :: 2',
+               'Programming Language :: Python :: 3',
+               'Programming Language :: Python :: 2.7',
+               'Programming Language :: Python :: 3.4',
+               'Programming Language :: Python :: 3.5',
+               'Programming Language :: Python :: 3.6']
 
 with open('requirements.txt') as f:
-    tests_require = f.readlines()
-install_requires = [t.strip() for t in tests_require]
+    requirements_lines = f.readlines()
+install_requires = [r.strip() for r in requirements_lines]
 
 # now call setup
 setup(name='osmnx',
@@ -52,4 +53,4 @@ setup(name='osmnx',
       platforms='any',
       packages=['osmnx'],
       install_requires=install_requires,
-      extras_require={'folium':['folium>=0.2']})
+      extras_require={'folium':['folium>=0.3']})
