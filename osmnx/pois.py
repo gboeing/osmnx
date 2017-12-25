d66cfb74ac8c3b55f9728b57230387466e69e7d1
Henrikki Tenkanen <henrikki.tenkanen@helsinki.fi>
2017-12-25 19:37:59 +0200

    Added functionality to parse relations.

--- osmnx/pois.py
+++ osmnx/pois.py
@@ -5,7 +5,7 @@
 # Web: https://github.com/gboeing/osmnx
 ################################################################################
 
-from shapely.geometry import Point, LineString, box
+from shapely.geometry import Point, LineString, MultiPolygon, Polygon, box
 import geopandas as gpd
 from .core import overpass_request, bbox_from_point, gdf_from_place
 from .utils import log, geocode, bbox_to_poly
@@ -127,6 +127,139 @@ def osm_poi_download(polygon=None, amenities=None, north=None, south=None, east=
 
     return responses
 
+def parse_vertice_nodes(osm_response):
+    """
+    Parse node vertices from OSM response.
+     
+    Parameters
+    ==========
+    osm_response : JSON 
+        OSM response JSON 
+    
+    Returns
+    =======
+    Dict of vertice IDs and their lat, lon coordinates.
+    """
+    vertices = {}
+    for result in osm_response['elements']:
+        if 'type' in result and result['type'] == 'node':
+            vertices[result['id']] = {'lat': result['lat'],
+                                      'lon': result['lon']}
+    return vertices
+
+def parse_osm_way(vertices, response):
+    """
+    Parse ways (areas) from OSM node vertices.
+
+    Parameters
+    ==========
+    vertices : Python dict 
+        Node vertices parsed from OSM response.  
+
+    Returns
+    =======
+    Dict of vertice IDs and their lat, lon coordinates.
+    """
+    if 'type' in response and response['type'] == 'way':
+        nodes = response['nodes']
+        try:
+            polygon = Polygon([(vertices[node]['lon'], vertices[node]['lat']) for node in nodes])
+
+            poi = {'nodes': nodes,
+                   'geometry': polygon}
+
+            if 'tags' in response:
+                for tag in response['tags']:
+                    poi[tag] = response['tags'][tag]
+            return poi
+
+        except Exception:
+            log('Polygon has invalid geometry: {}'.format(nodes))
+    return None
+
+def parse_osm_node(response):
+    """
+    Parse points from OSM nodes.
+
+    Parameters
+    ==========
+    response : JSON 
+        Nodes from OSM response.  
+
+    Returns
+    =======
+    Dict of vertice IDs and their lat, lon coordinates.
+    """
+    try:
+        point = Point(response['lon'], response['lat'])
+
+        poi = {
+            'osmid': response['id'],
+            'geometry': point
+        }
+        if 'tags' in response:
+            for tag in response['tags']:
+                poi[tag] = response['tags'][tag]
+    except Exception:
+        log('Point has invalid geometry: {}'.format(response['id']))
+
+    return poi
+
+def parse_osm_relations(relations, osm_way_df):
+    """
+    Parses the osm relations from osm ways and nodes.
+         
+    Parameters
+    ==========
+    relations : list
+        OSM 'relation' items (dictionaries) in a list. 
+    osm_way_gdf : gpd.GeoDataFrame
+        OSM 'way' features as a GeoDataFrame that contains all the 'way' features that will constitute the 'relations' .
+     
+    Information
+    ===========
+    http://wiki.openstreetmap.org/wiki/Relation 
+    
+    Returns
+    =======
+    gpd.GeoDataFrame
+        A GeoDataFrame with MultiPolygon representations of the relations and the attributes associated with them.   
+    """
+    gdf_relations = gpd.GeoDataFrame()
+
+    # Iterate over relations and extract the items
+    for relation in relations:
+        if relation['tags']['type'] == 'multipolygon':
+            # Parse member 'way' ids
+            member_way_ids = [member['ref'] for member in relation['members'] if member['type'] == 'way']
+            # Extract the ways
+            member_ways = osm_way_df.loc[member_way_ids]
+            # Extract the nodes of those ways
+            member_nodes = list(member_ways['nodes'].values)
+            # Create MultiPolygon
+            multipoly = MultiPolygon(list(member_ways['geometry'].values))
+            # Create GeoDataFrame with the tags and the MultiPolygon and its 'ways' (ids), and the 'nodes' of those ways
+            geo = gpd.GeoDataFrame(relation['tags'], index=[relation['id']])
+            geo['geometry'] = None
+            geo['ways'] = None
+            geo['nodes'] = None
+            geo.loc[relation['id'], 'geometry'] = multipoly
+            geo.loc[relation['id'], 'ways'] = member_way_ids
+            geo.loc[relation['id'], 'nodes'] = member_nodes
+
+            # Append to relation GeoDataFrame
+            gdf_relations = gdf_relations.append(geo)
+            # Remove such 'ways' from 'osm_way_df' that are part of the 'relation'
+            osm_way_df = osm_way_df.drop(member_way_ids)
+        elif relation['type'] == 'multiline':
+            print("TODO: multiline relation")
+        elif relation['type'] == 'multipoint':
+            print("TODO: multipoint relation")
+
+    # Merge 'osm_way_df' and the 'gdf_relations'
+    osm_way_df = osm_way_df.append(gdf_relations)
+    return osm_way_df
+
 def create_poi_gdf(polygon=None, amenities=None, north=None, south=None, east=None, west=None, retain_invalid=False):
     """
     Parse GeoDataFrames from POI json that was returned by Overpass API.
@@ -154,35 +287,42 @@ def create_poi_gdf(polygon=None, amenities=None, north=None, south=None, east=No
     """
     responses = osm_poi_download(polygon=polygon, amenities=amenities, north=north, south=south, east=east, west=west)
 
-    # Parse POIs
+    # Parse vertices
+    vertices = parse_vertice_nodes(responses)
+
+    # POIs
     pois = {}
+
+    # POI ways
+    poi_ways = {}
+
+    # A list of POI relations
+    relations = []
+
     for result in responses['elements']:
         if result['type'] == 'node' and 'tags' in result:
-            try:
-                point = Point(result['lon'], result['lat'])
-
-                poi = {
-                    'osmid': result['id'],
-                    'geometry': point
-                }
-                if 'tags' in result:
-                    for tag in result['tags']:
-                        poi[tag] = result['tags'][tag]
-                pois[result['id']] = poi
-
-            except Exception:
-                log('Point has invalid geometry: {}'.format(result['id']))
-
-        elif result['type'] == 'relation':
-            # TODO: Add functionalities to parse 'relation' tags.
-            pass
+            poi = parse_osm_node(response=result)
+            pois[result['id']] = poi
         elif result['type'] == 'way':
-            # TODO: Add functionalities to parse 'way' tags as a separate GeoDataFrame.
-            pass
+            # Parse POI area Polygon
+            poi_area = parse_osm_way(vertices=vertices, response=result)
+            if poi_area:
+                poi_ways[result['id']] = poi_area
+        elif result['type'] == 'relation':
+            # Add relation to a relation list (needs to be parsed after all nodes and ways have been parsed)
+            relations.append(result)
 
-    gdf = gpd.GeoDataFrame(pois).T
-    gdf.crs = {'init': 'epsg:4326'}
-    return gdf
+    # Create GeoDataFrames
+    gdf_nodes = gpd.GeoDataFrame(pois).T
+    gdf_nodes.crs = {'init': 'epsg:4326'}
+
+    gdf_ways = gpd.GeoDataFrame(poi_ways).T
+    gdf_ways.crs = {'init': 'epsg:4326'}
+
+    # Parse relations (MultiPolygons) from 'ways'
+    gdf_ways = parse_osm_relations(relations=relations, osm_way_df=gdf_ways)
+
+    return (gdf_nodes, gdf_ways)
 
 def pois_from_point(point, distance=None, amenities=None, retain_invalid=False):
     """
