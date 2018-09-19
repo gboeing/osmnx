96a2ee369b0f67becfc28826e834bbb4300d3256
Geoff Boeing <gboeing@berkeley.edu>
2018-09-19 14:51:44 -0400

    fix classifiers list

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ classifiers = ['Development Status :: 5 - Production/Stable',
                'Programming Language :: Python :: 3',
                'Programming Language :: Python :: 3.4',
                'Programming Language :: Python :: 3.5',
-               'Programming Language :: Python :: 3.6'
+               'Programming Language :: Python :: 3.6',
                'Programming Language :: Python :: 3.7']
 
 with open('requirements.txt') as f:
