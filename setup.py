d0b6e840ba561d4a014fa3b434f37c880189c931
gboeing <g.boeing@northeastern.edu>
2019-01-28 16:00:41 -0500

    dev version

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ install_requires = [r.strip() for r in requirements_lines]
 
 # now call setup
 setup(name='osmnx',
-      version='0.9',
+      version='0.10dev',
       description='Retrieve, construct, analyze, and visualize street networks from OpenStreetMap',
       long_description=long_description,
       classifiers=classifiers,
