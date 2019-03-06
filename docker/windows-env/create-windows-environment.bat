fade87578eb8741168504df387a82e38e5d91ac3
gboeing <g.boeing@northeastern.edu>
2019-03-06 13:26:22 -0500

    add windows env batch

---
+++ docker/windows-env/create-windows-environment.bat
@@ -0,0 +1,11 @@
+CALL conda update conda -n base --yes
+CALL conda deactivate
+CALL conda env remove -n ox --yes
+CALL conda create -n ox -c conda-forge --file "../../requirements.txt" --file "../../requirements-dev.txt" --file "requirements-more.txt" --yes
+CALL conda activate ox
+CALL python -m ipykernel install --user
+CALL jupyter labextension install @jupyter-widgets/jupyterlab-manager
+CALL jupyter labextension install jupyter-leaflet
+CALL pip install https://ocf.berkeley.edu/~gboeing/share/python_igraph-0.7.1.post6-cp37-cp37m-win_amd64.whl
+CALL conda clean --all --yes
+CALL conda env export > environment-windows.yml
