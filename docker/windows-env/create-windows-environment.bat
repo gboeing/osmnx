583959516badba1d5824386ae17ce6e2be2b1932
gboeing <g.boeing@northeastern.edu>
2019-03-13 12:49:59 -0400

    update env

--- docker/windows-env/create-windows-environment.bat
+++ docker/windows-env/create-windows-environment.bat
@@ -3,10 +3,12 @@ CALL conda deactivate
 CALL conda env remove -n ox --yes
 CALL conda create -n ox -c conda-forge --file "../../requirements.txt" --file "../../requirements-dev.txt" --file "requirements-more.txt" --yes
 CALL conda activate ox
-CALL python -m ipykernel install --user
+CALL python -m ipykernel install --user --name ox --display-name "Python (ox)"
 CALL jupyter labextension install @jupyter-widgets/jupyterlab-manager
 CALL jupyter labextension install jupyter-leaflet
 CALL pip install https://ocf.berkeley.edu/~gboeing/share/python_igraph-0.7.1.post6-cp37-cp37m-win_amd64.whl
 CALL conda clean --all --yes
 CALL conda env export > environment-windows.yml
+CALL conda list
+CALL jupyter kernelspec list
 CALL python -c "import osmnx; print(osmnx.__version__)"
