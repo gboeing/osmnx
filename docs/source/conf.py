5a622e9b0a5b49c1c5a85229e9d6346463aafdae
gboeing <g.boeing@northeastern.edu>
2019-12-03 19:23:08 -0800

    rtd

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,6 +25,8 @@ sys.path.insert(0, os.path.abspath('../..'))
 
 # mock import these packages because readthedocs doesn't have them installed
 autodoc_mock_imports = [
+  'ctypes',
+  'ctypes.util',
   'dateutil',
   'descartes',
   'geopandas',
