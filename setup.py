6dec70dd2c36bf99d300c1a30dc60ee9d49243fd
gboeing <g.boeing@northeastern.edu>
2019-12-04 09:22:26 -0800

    version bump

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ else:
 
 # now call setup
 setup(name='osmnx',
-      version='0.11dev',
+      version='0.11',
       description='Retrieve, model, analyze, and visualize OpenStreetMap street networks and other spatial data',
       long_description=LONG_DESCRIPTION,
       classifiers=CLASSIFIERS,
