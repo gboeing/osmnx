af5360b5ee36d2f71c97bf63d9b1a49088f2bb5d
pablo <pablomartincalvo@gmail.com>
2020-03-21 18:36:09 +0100

    Dynamic settings in osm_footprints_download.

--- osmnx/footprints.py
+++ osmnx/footprints.py
@@ -29,7 +29,7 @@ from .geo_utils import geocode
 
 def osm_footprints_download(polygon=None, north=None, south=None, east=None, west=None,
                             footprint_type='building', timeout=180, memory=None,
-                            max_query_area_size=50*1000*50*1000):
+                            max_query_area_size=50*1000*50*1000, custom_settings= None):
     """
     Download OpenStreetMap footprint data as a list of json responses.
 
@@ -57,6 +57,9 @@ def osm_footprints_download(polygon=None, north=None, south=None, east=None, wes
         any polygon bigger will get divided up for multiple queries to API
         (default is 50,000 * 50,000 units (ie, 50km x 50km in area, if units are
         meters))
+    custom_settings : string
+        custom settings to be used in the overpass query instead of the default
+        ones
 
     Returns
     -------
@@ -82,6 +85,12 @@ def osm_footprints_download(polygon=None, north=None, south=None, east=None, wes
     else:
         maxsize = '[maxsize:{}]'.format(memory)
 
+    # use custom settings if delivered, otherwise just the default ones.
+    if custom_settings:
+        overpass_settings = custom_settings
+    else:
+        overpass_settings = '[out:json][timeout:{timeout}]{maxsize}'.format(timeout=timeout, maxsize=maxsize)
+
     # define the query to send the API
     if by_bbox:
         # turn bbox into a polygon and project to local UTM
@@ -102,13 +111,13 @@ def osm_footprints_download(polygon=None, north=None, south=None, east=None, wes
             # decimal places (ie, within 1 mm) so URL strings aren't different
             # due to float rounding issues (for consistent caching)
             west, south, east, north = poly.bounds
-            query_template = ('[out:json][timeout:{timeout}]{maxsize};'
+            query_template = ('{settings};'
                               '((way["{footprint_type}"]({south:.8f},{west:.8f},{north:.8f},{east:.8f});'
                               '(._;>;););'
                               '(relation["{footprint_type}"]({south:.8f},{west:.8f},{north:.8f},{east:.8f});'
                               '(._;>;);););out;')
             query_str = query_template.format(north=north, south=south, east=east, west=west, timeout=timeout,
-                                              maxsize=maxsize, footprint_type=footprint_type)
+                                              maxsize=maxsize, footprint_type=footprint_type, settings=overpass_settings)
             response_json = overpass_request(data={'data':query_str}, timeout=timeout)
             response_jsons.append(response_json)
         msg = ('Got all footprint data within bounding box from '
@@ -128,11 +137,11 @@ def osm_footprints_download(polygon=None, north=None, south=None, east=None, wes
         # pass each polygon exterior coordinates in the list to the API, one at
         # a time
         for polygon_coord_str in polygon_coord_strs:
-            query_template = ('[out:json][timeout:{timeout}]{maxsize};('
+            query_template = ('{settings};('
                               'way(poly:"{polygon}")["{footprint_type}"];(._;>;);'
                               'relation(poly:"{polygon}")["{footprint_type}"];(._;>;););out;')
             query_str = query_template.format(polygon=polygon_coord_str, timeout=timeout, maxsize=maxsize,
-                                              footprint_type=footprint_type)
+                                              footprint_type=footprint_type, settings=overpass_settings)
             response_json = overpass_request(data={'data':query_str}, timeout=timeout)
             response_jsons.append(response_json)
         msg = ('Got all footprint data within polygon from API in '
