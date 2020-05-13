5113b81cd0c43b50ccbd9755662d779f4dc10cbe
gboeing <boeing@usc.edu>
2020-05-12 18:33:49 -0700

    update tests

--- tests/test_osmnx.py
+++ tests/test_osmnx.py
@@ -135,6 +135,9 @@ def test_stats():
     # calculate extended stats
     stats4 = ox.extended_stats(G, connectivity=True, anc=False, ecc=True, bc=True, cc=True)
 
+    # test cleaning and rebuilding graph
+    G_clean = ox.clean_intersections(G_proj, tolerance=10, rebuild_graph=True, dead_ends=True)
+
 
 
 def test_graph_from_file():
@@ -471,28 +474,3 @@ def test_overpass_endpoint():
         G = ox.graph_from_place('Piedmont, California, USA')
 
     ox.config(overpass_endpoint='http://overpass-api.de/api')
-
-def test_coarse_graining():
-    T = nx.MultiDiGraph()
-    T.add_node(1, osmid=1, y=52.528842, x=13.397922)
-    T.add_node(2, osmid=2, y=52.528877, x=13.398594)
-    T.add_node(3, osmid=3, y=52.527467, x=13.399258)
-
-    T.add_edge(1, 2, osmid=100, length=45.5)
-    T.add_edge(2, 3, osmid=101, length=162.82)
-    T.add_edge(1, 3, osmid=102, length=177.60)
-    T.graph['name'] = 'test'
-    T.graph['crs'] = '+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs'
-
-    S = ox.simplify.coarse_grain_osm_network(T, tolerance=10)
-    # coarse graining should have no effect since tolerance is too low
-    assert S.number_of_nodes() == 3
-    assert S.number_of_edges() == 3
-    S = ox.simplify.coarse_grain_osm_network(T, tolerance=30)
-    # coarse graining should collapse 1 and 2
-    assert S.number_of_nodes() == 2
-    assert S.number_of_edges() == 1
-    S = ox.simplify.coarse_grain_osm_network(T, tolerance=600)
-    # coarse graining should collapse all edges
-    assert S.number_of_nodes() == 1
-    assert S.number_of_edges() == 0
