aad969d238c427e9886b77ebd47f6671e2f194e3
gboeing <boeing@usc.edu>
2020-05-13 20:21:21 -0700

    move dist functions to geo_utils module

--- osmnx/geo_utils.py
+++ osmnx/geo_utils.py
@@ -44,6 +44,70 @@ except ImportError as e:
     BallTree = None
 
 
+
+def great_circle_vec(lat1, lng1, lat2, lng2, earth_radius=6371009):
+    """
+    Vectorized function to calculate the great-circle distance between two
+    points or between vectors of points, using haversine.
+
+    Parameters
+    ----------
+    lat1 : float or array of float
+    lng1 : float or array of float
+    lat2 : float or array of float
+    lng2 : float or array of float
+    earth_radius : numeric
+        radius of earth in units in which distance will be returned (default is
+        meters)
+
+    Returns
+    -------
+    distance : float or vector of floats
+        distance or vector of distances from (lat1, lng1) to (lat2, lng2) in
+        units of earth_radius
+    """
+
+    phi1 = np.deg2rad(lat1)
+    phi2 = np.deg2rad(lat2)
+    d_phi = phi2 - phi1
+
+    theta1 = np.deg2rad(lng1)
+    theta2 = np.deg2rad(lng2)
+    d_theta = theta2 - theta1
+
+    h = np.sin(d_phi / 2) ** 2 + np.cos(phi1) * np.cos(phi2) * np.sin(d_theta / 2) ** 2
+    h = np.minimum(1.0, h)  # protect against floating point errors
+
+    arc = 2 * np.arcsin(np.sqrt(h))
+
+    # return distance in units of earth_radius
+    distance = arc * earth_radius
+    return distance
+
+
+def euclidean_dist_vec(y1, x1, y2, x2):
+    """
+    Vectorized function to calculate the euclidean distance between two points
+    or between vectors of points.
+
+    Parameters
+    ----------
+    y1 : float or array of float
+    x1 : float or array of float
+    y2 : float or array of float
+    x2 : float or array of float
+
+    Returns
+    -------
+    distance : float or array of float
+        distance or vector of distances from (x1, y1) to (x2, y2) in graph units
+    """
+
+    # euclid's formula
+    distance = ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
+    return distance
+
+
 def induce_subgraph(G, node_subset):
     """
     Induce a subgraph of G.
