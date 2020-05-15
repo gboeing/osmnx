06aa396510d2e42cfc1a8a6b6e361fdee368b177
gboeing <boeing@usc.edu>
2020-05-15 16:53:50 -0700

    refactor get_undirected and related functions from save_load to utils_graph

--- osmnx/utils_graph.py
+++ osmnx/utils_graph.py
@@ -24,13 +24,13 @@ def induce_subgraph(G, node_subset):
 
     Parameters
     ----------
-    G : networkx multidigraph
+    G : networkx.MultiDiGraph
     node_subset : list-like
         the subset of nodes to induce a subgraph of G
 
     Returns
     -------
-    H : networkx multidigraph
+    H : networkx.MultiDiGraph
         the subgraph of G induced by node_subset
     """
 
@@ -64,14 +64,14 @@ def get_largest_component(G, strongly=False):
 
     Parameters
     ----------
-    G : networkx multidigraph
+    G : networkx.MultiDiGraph
     strongly : bool
         if True, return the largest strongly instead of weakly connected
         component
 
     Returns
     -------
-    G : networkx multidigraph
+    G : networkx.MultiDiGraph
         the largest connected component subgraph from the original graph
     """
 
@@ -106,7 +106,8 @@ def get_largest_component(G, strongly=False):
 
 
 
-def get_route_edge_attributes(G, route, attribute=None, minimize_key='length', retrieve_default=None):
+def get_route_edge_attributes(G, route, attribute=None, minimize_key='length',
+	                          retrieve_default=None):
     """
     Get a list of attribute values for each edge in a path.
 
@@ -122,8 +123,9 @@ def get_route_edge_attributes(G, route, attribute=None, minimize_key='length', r
         if there are parallel edges between two nodes, select the one with the
         lowest value of minimize_key
     retrieve_default : Callable[Tuple[Any, Any], Any]
-        Function called with the edge nodes as parameters to retrieve a default value, if the edge does not
-        contain the given attribute. Per default, a `KeyError` is raised
+        Function called with the edge nodes as parameters to retrieve a
+        default value, if the edge does not contain the given attribute. Per
+        default, a `KeyError` is raised
     Returns
     -------
     attribute_values : list
@@ -148,20 +150,21 @@ def get_route_edge_attributes(G, route, attribute=None, minimize_key='length', r
 
 def count_streets_per_node(G, nodes=None):
     """
-    Count how many street segments emanate from each node (i.e., intersections and dead-ends) in this graph.
-
-    If nodes is passed, then only count the nodes in the graph with those IDs.
+    Count how many street segments emanate from each node (i.e.,
+    intersections and dead-ends) in this graph. If nodes is passed,
+    then only count the nodes in the graph with those IDs.
 
     Parameters
     ----------
-    G : networkx multidigraph
+    G : networkx.MultiDiGraph
     nodes : iterable
         the set of node IDs to get counts for
 
     Returns
     ----------
     streets_per_node : dict
-        counts of how many streets emanate from each node with keys=node id and values=count
+        counts of how many streets emanate from each node with
+        keys=node id and values=count
     """
 
     # to calculate the counts, get undirected representation of the graph. for
@@ -217,7 +220,7 @@ def graph_to_gdfs(G, nodes=True, edges=True, node_geometry=True, fill_edge_geome
 
     Parameters
     ----------
-    G : networkx multidigraph
+    G : networkx.MultiDiGraph
     nodes : bool
         if True, convert graph nodes to a GeoDataFrame and return it
     edges : bool
@@ -292,7 +295,7 @@ def graph_to_gdfs(G, nodes=True, edges=True, node_geometry=True, fill_edge_geome
 
 def gdfs_to_graph(gdf_nodes, gdf_edges):
     """
-    Convert node and edge GeoDataFrames into a graph
+    Convert node and edge GeoDataFrames into a MultiDiGraph
 
     Parameters
     ----------
@@ -301,7 +304,7 @@ def gdfs_to_graph(gdf_nodes, gdf_edges):
 
     Returns
     -------
-    networkx multidigraph
+    networkx.MultiDiGraph
     """
 
     G = nx.MultiDiGraph()
@@ -335,15 +338,215 @@ def remove_isolated_nodes(G):
 
     Parameters
     ----------
-    G : networkx multidigraph
+    G : networkx.MultiDiGraph
         the graph from which to remove nodes
 
     Returns
     -------
-    networkx multidigraph
+    networkx.MultiDiGraph
     """
 
     isolated_nodes = [node for node, degree in dict(G.degree()).items() if degree < 1]
     G.remove_nodes_from(isolated_nodes)
     utils.log(f'Removed {len(isolated_nodes)} isolated nodes')
     return G
+
+
+
+def is_duplicate_edge(data, data_other):
+    """
+    Check if two edge data dictionaries are the same based on OSM ID and
+    geometry.
+
+    Parameters
+    ----------
+    data : dict
+        the first edge's data
+    data_other : dict
+        the second edge's data
+
+    Returns
+    -------
+    is_dupe : bool
+    """
+
+    is_dupe = False
+
+    # if either edge's OSM ID contains multiple values (due to simplification), we want
+    # to compare as sets so they are order-invariant, otherwise uv does not match vu
+    osmid = set(data['osmid']) if isinstance(data['osmid'], list) else data['osmid']
+    osmid_other = set(data_other['osmid']) if isinstance(data_other['osmid'], list) else data_other['osmid']
+
+    if osmid == osmid_other:
+        # if they contain the same OSM ID or set of OSM IDs (due to simplification)
+        if ('geometry' in data) and ('geometry' in data_other):
+            # if both edges have a geometry attribute
+            if is_same_geometry(data['geometry'], data_other['geometry']):
+                # if their edge geometries have the same coordinates
+                is_dupe = True
+        elif ('geometry' in data) and ('geometry' in data_other):
+            # if neither edge has a geometry attribute
+            is_dupe = True
+        else:
+            # if one edge has geometry attribute but the other doesn't, keep it
+            pass
+
+    return is_dupe
+
+
+
+def is_same_geometry(ls1, ls2):
+    """
+    Check if LineString geometries in two edges are the same, in
+    normal or reversed order of points.
+
+    Parameters
+    ----------
+    ls1 : LineString
+        the first edge's geometry
+    ls2 : LineString
+        the second edge's geometry
+
+    Returns
+    -------
+    bool
+    """
+
+    # extract geometries from each edge data dict
+    geom1 = [list(coords) for coords in ls1.xy]
+    geom2 = [list(coords) for coords in ls2.xy]
+
+    # reverse the first edge's list of x's and y's to look for a match in
+    # either order
+    geom1_r = [list(reversed(list(coords))) for coords in ls1.xy]
+
+    # if the edge's geometry matches its reverse's geometry in either order,
+    # return True
+    return (geom1 == geom2 or geom1_r == geom2)
+
+
+
+def update_edge_keys(G):
+    """
+    Update the keys of edges that share a u, v with another edge but differ in
+    geometry. For example, two one-way streets from u to v that bow away from
+    each other as separate streets, rather than opposite direction edges of a
+    single street.
+
+    Parameters
+    ----------
+    G : networkx.MultiDiGraph
+
+    Returns
+    -------
+    networkx.MultiDiGraph
+    """
+
+    # identify all the edges that are duplicates based on a sorted combination
+    # of their origin, destination, and key. that is, edge uv will match edge vu
+    # as a duplicate, but only if they have the same key
+    edges = graph_to_gdfs(G, nodes=False, fill_edge_geometry=False)
+    edges['uvk'] = edges.apply(lambda row: '_'.join(sorted([str(row['u']), str(row['v'])]) + [str(row['key'])]), axis=1)
+    edges['dupe'] = edges['uvk'].duplicated(keep=False)
+    dupes = edges[edges['dupe']==True].dropna(subset=['geometry'])
+
+    different_streets = []
+    groups = dupes[['geometry', 'uvk', 'u', 'v', 'key', 'dupe']].groupby('uvk')
+
+    # for each set of duplicate edges
+    for label, group in groups:
+
+        # if there are more than 2 edges here, make sure to compare all
+        if len(group['geometry']) > 2:
+            l = group['geometry'].tolist()
+            l.append(l[0])
+            geom_pairs = list(zip(l[:-1], l[1:]))
+        # otherwise, just compare the first edge to the second edge
+        else:
+            geom_pairs = [(group['geometry'].iloc[0], group['geometry'].iloc[1])]
+
+        # for each pair of edges to compare
+        for geom1, geom2 in geom_pairs:
+            # if they don't have the same geometry, flag them as different streets
+            if not is_same_geometry(geom1, geom2):
+                # add edge uvk, but not edge vuk, otherwise we'll iterate both their keys
+                # and they'll still duplicate each other at the end of this process
+                different_streets.append((group['u'].iloc[0], group['v'].iloc[0], group['key'].iloc[0]))
+
+    # for each unique different street, iterate its key + 1 so it's unique
+    for u, v, k in set(different_streets):
+        # filter out key if it appears in data dict as we'll pass it explicitly
+        attributes = {k:v for k, v in G[u][v][k].items() if k != 'key'}
+        G.add_edge(u, v, key=k+1, **attributes)
+        G.remove_edge(u, v, key=k)
+
+    return G
+
+
+
+def get_undirected(G):
+    """
+    Convert a directed graph to an undirected graph that maintains parallel
+    edges if geometries differ.
+
+    Parameters
+    ----------
+    G : networkx.MultiDiGraph
+
+    Returns
+    -------
+    networkx.MultiGraph
+    """
+
+    # set from/to nodes before making graph undirected
+    G = G.copy()
+    for u, v, k, data in G.edges(keys=True, data=True):
+        G.edges[u, v, k]['from'] = u
+        G.edges[u, v, k]['to'] = v
+
+        # add geometry if it doesn't already exist, to retain parallel
+        # edges' distinct geometries
+        if 'geometry' not in data:
+            point_u = Point((G.nodes[u]['x'], G.nodes[u]['y']))
+            point_v = Point((G.nodes[v]['x'], G.nodes[v]['y']))
+            data['geometry'] = LineString([point_u, point_v])
+
+    # update edge keys so we don't retain only one edge of sets of parallel edges
+    # when we convert from a multidigraph to a multigraph
+    G = update_edge_keys(G)
+
+    # now convert multidigraph to a multigraph, retaining all edges in both
+    # directions for now, as well as all graph attributes
+    H = nx.MultiGraph()
+    H.add_nodes_from(G.nodes(data=True))
+    H.add_edges_from(G.edges(keys=True, data=True))
+    H.graph = G.graph
+    H.name = G.name
+
+    # the previous operation added all directed edges from G as undirected
+    # edges in H. this means we have duplicate edges for every bi-directional
+    # street. so, look through the edges and remove any duplicates
+    duplicate_edges = []
+    for u, v, key, data in H.edges(keys=True, data=True):
+
+        # if we haven't already flagged this edge as a duplicate
+        if not (u, v, key) in duplicate_edges:
+
+            # look at every other edge between u and v, one at a time
+            for key_other in H[u][v]:
+
+                # don't compare this edge to itself
+                if not key_other == key:
+
+                    # compare the first edge's data to the second's to see if
+                    # they are duplicates
+                    data_other = H.edges[u, v, key_other]
+                    if is_duplicate_edge(data, data_other):
+
+                        # if they match up, flag the duplicate for removal
+                        duplicate_edges.append((u, v, key_other))
+
+    H.remove_edges_from(duplicate_edges)
+    utils.log('Made undirected graph')
+
+    return H
