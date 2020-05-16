c5827e50fe282767f19aeb5565a8a8e5986888a9
gboeing <boeing@usc.edu>
2020-05-15 17:04:37 -0700

    add api module

---
+++ osmnx/api.py
@@ -0,0 +1,20 @@
+################################################################################
+# Module: api.py
+# Description: The OSMnx API
+# License: MIT, see full license in LICENSE.txt
+# Web: https://github.com/gboeing/osmnx
+################################################################################
+
+from .core import *
+from .downloader import *
+from .elevation import *
+from .footprints import *
+from .plot import *
+from .pois import *
+from .projection import *
+from .save_load import *
+from .simplification import *
+from .stats import *
+from .utils import *
+from .utils_geo import *
+from .utils_graph import *
