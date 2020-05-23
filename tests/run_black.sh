fa8c8cebd7a24b3cc96c8a22486655870544e0a7
gboeing <boeing@usc.edu>
2020-05-22 21:04:51 -0700

    simplify script

--- tests/run_black.sh
+++ tests/run_black.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-black --line-length 100 ./setup.py
-black --line-length 100 ./docs/source/conf.py
-black --line-length 100 ./tests/test_osmnx.py
-black --line-length 100 ./osmnx/*
+black . --line-length 100
