dcd09de6538c85f60e116e8d294397134d081f5b
Geoff Boeing <boeing@usc.edu>
2020-06-06 16:03:38 -0700

    version bump

--- osmnx/_version.py
+++ osmnx/_version.py
@@ -1,3 +1,3 @@
 """OSMnx package version."""
 
-__version__ = "0.14.0"
+__version__ = "0.14.1"
--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ else:
 # now call setup
 setup(
     name="osmnx",
-    version="0.14.0",
+    version="0.14.1",
     description=DESC,
     long_description=LONG_DESCRIPTION,
     classifiers=CLASSIFIERS,
