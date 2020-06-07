6b4edf3fa06bce411bf27d6498f574c16ef8c3aa
Geoff Boeing <boeing@usc.edu>
2020-06-06 23:08:54 -0700

    improve projection

--- osmnx/projection.py
+++ osmnx/projection.py
@@ -11,28 +11,6 @@ from . import settings
 from . import utils
 
 
-def _is_crs_utm(crs):
-    """
-    Determine if a CRS is a UTM CRS.
-
-    Parameters
-    ----------
-    crs : dict or string or pyproj.CRS
-        a coordinate reference system
-
-    Returns
-    -------
-    bool
-        True if crs is UTM, False otherwise
-    """
-    if not crs:
-        return False
-    crs_obj = CRS.from_user_input(crs)
-    if crs_obj.coordinate_operation and crs_obj.coordinate_operation.name.upper().startswith("UTM"):
-        return True
-    return False
-
-
 def project_geometry(geometry, crs=None, to_crs=None, to_latlong=False):
     """
     Project a shapely geometry from its current CRS to another.
@@ -51,7 +29,7 @@ def project_geometry(geometry, crs=None, to_crs=None, to_latlong=False):
         if None, project to UTM zone in which geometry's centroid lies,
         otherwise project to this CRS
     to_latlong : bool
-        if True, project to settings.default_crs and ignore to_crs
+        if True, project to settings.default_crs
 
     Returns
     -------
@@ -88,46 +66,42 @@ def project_gdf(gdf, to_crs=None, to_latlong=False):
         if None, project to UTM zone in which gdf's centroid lies, otherwise
         project to this CRS
     to_latlong : bool
-        if True, project to settings.default_crs and ignore to_crs
+        if True, project to settings.default_crs
 
     Returns
     -------
     gdf_proj : geopandas.GeoDataFrame
         the projected GeoDataFrame
     """
-    if len(gdf) < 1:
-        raise ValueError("Cannot project an empty GeoDataFrame")
+    if gdf.crs is None or len(gdf) < 1:
+        raise ValueError("GeoDataFrame cannot be empty and must have a valid CRS")
 
-    # if to_crs was passed-in, use this value to project the gdf
-    if to_crs is not None:
+    # if to_latlong is True, project the gdf to latlong
+    if to_latlong:
+        latlong_crs = settings.default_crs
+        gdf_proj = gdf.to_crs(latlong_crs)
+        utils.log("Projected GeoDataFrame to settings.default_crs")
+
+    # else if to_crs was passed-in, project gdf to this CRS
+    elif to_crs is not None:
         gdf_proj = gdf.to_crs(to_crs)
+        utils.log(f"Projected GeoDataFrame to {to_crs}")
 
-    # if to_crs was not passed-in, calculate the centroid of the geometry to
-    # determine UTM zone
+    # otherwise, automatically project the gdf to UTM
     else:
-        if to_latlong:
-            # if to_latlong is True, project the gdf to latlong
-            latlong_crs = settings.default_crs
-            gdf_proj = gdf.to_crs(latlong_crs)
-            utils.log("Projected GeoDataFrame to settings.default_crs")
-        else:
-            # else, project the gdf to UTM
-            # if GeoDataFrame is already in UTM, just return it
-            if _is_crs_utm(gdf.crs):
-                return gdf
+        if CRS.from_user_input(gdf.crs).is_projected:
+            raise ValueError("Geometry must be unprojected to calculate UTM zone")
 
-            # calculate the centroid of the union of all the geometries in the
-            # GeoDataFrame
-            avg_longitude = gdf["geometry"].unary_union.centroid.x
+        # calculate longitude of centroid of union of all geometries in gdf
+        avg_lng = gdf["geometry"].unary_union.centroid.x
 
-            # calculate the UTM zone from this avg longitude and define the UTM
-            # CRS to project
-            utm_zone = int(math.floor((avg_longitude + 180) / 6.0) + 1)
-            utm_crs = f"+proj=utm +zone={utm_zone} +ellps=WGS84 +datum=WGS84 +units=m +no_defs"
+        # calculate UTM zone from avg longitude to define CRS to project to
+        utm_zone = int(math.floor((avg_lng + 180) / 6.0) + 1)
+        utm_crs = f"+proj=utm +zone={utm_zone} +ellps=WGS84 +datum=WGS84 +units=m +no_defs"
 
-            # project the GeoDataFrame to the UTM CRS
-            gdf_proj = gdf.to_crs(utm_crs)
-            utils.log(f"Projected GeoDataFrame to UTM-{utm_zone}")
+        # project the GeoDataFrame to the UTM CRS
+        gdf_proj = gdf.to_crs(utm_crs)
+        utils.log(f"Projected GeoDataFrame to UTM-{utm_zone}")
 
     return gdf_proj
 
