dbf5f3e3b7be9064e41d2d7d7e8c7517fe30e858
Geoff Boeing <boeing@usc.edu>
2020-06-19 22:20:03 -0700

    add geocode_to_gdf function

--- osmnx/geocoding.py
+++ osmnx/geocoding.py
@@ -46,19 +46,22 @@ def geocode(query):
         raise Exception(f'Nominatim geocoder returned no results for query "{query}"')
 
 
-def gdf_from_place(query, which_result=1, buffer_dist=None):
+def geocode_to_gdf(query, which_result=1, buffer_dist=None):
     """
-    Create a GeoDataFrame from a single place name query.
+    Geocode a query or queries to a GeoDataFrame with the Nominatim geocoder.
 
-    Geocode the query with Nominatim then turn it into a GeoDataFrame with
-    a geometry column.
+    Geometry column contains place boundaries if they exist in OpenStreetMap.
+    Query can be a string or dict, or a list of strings/dicts to send to the
+    geocoder. If query is a list, then which_result should be a list of the
+    same length.
 
     Parameters
     ----------
-    query : string or dict
-        query string or structured query dict to geocode/download
-    which_result : int
-        max number of results to return and which to process upon receipt
+    query : string or dict or list
+        query string or structured dict to geocode/download
+    which_result : int or list
+        max number of results to return and which to process upon receipt; if
+        passing a list then it must be same length as query
     buffer_dist : float
         distance to buffer around the place geometry, in meters
 
@@ -66,14 +69,66 @@ def gdf_from_place(query, which_result=1, buffer_dist=None):
     -------
     gdf : geopandas.GeoDataFrame
     """
+    # if caller passed a list of queries but a scalar which_result value, then
+    # turn which_result into a list with same length as query list
+    if isinstance(query, list) and isinstance(which_result, int):
+        which_result = [which_result] * len(query)
+
+    # turn query and which_result into lists if they're not already
+    if not isinstance(query, list):
+        query = [query]
+    if not isinstance(which_result, list):
+        which_result = [which_result]
+
+    # ensure same length
+    if len(query) != len(which_result):
+        raise ValueError("which_result length must equal query length")
+
     # ensure query type
-    if not isinstance(query, (str, dict)):
-        raise ValueError("query must be a dict or a string")
+    for q in query:
+        if not isinstance(q, (str, dict)):
+            raise ValueError("each query must be a dict or a string")
 
-    # get the data from OSM
+    # geocode each query and add to GeoDataFrame as a new row
+    gdf = gpd.GeoDataFrame()
+    for q, wr in zip(query, which_result):
+        gdf_tmp = _geocode_query_to_gdf(q, wr)
+        gdf = gdf.append(gdf_tmp)
+
+    # reset GeoDataFrame index and set its CRS
+    gdf = gdf.reset_index(drop=True)
+    gdf.crs = settings.default_crs
+
+    # if buffer_dist was passed in, project the geometry to UTM, buffer it in
+    # meters, then project it back to lat-lng
+    if buffer_dist is not None and len(gdf) > 0:
+        gdf_utm = projection.project_gdf(gdf)
+        gdf_utm["geometry"] = gdf_utm["geometry"].buffer(buffer_dist)
+        gdf = projection.project_gdf(gdf_utm, to_latlong=True)
+        utils.log(f"Buffered GeoDataFrame to {buffer_dist} meters")
+
+    utils.log(f"Created GeoDataFrame with {len(gdf)} rows from {len(query)} queries")
+    return gdf
+
+
+def _geocode_query_to_gdf(query, which_result=1):
+    """
+    Geocode a single place query to a GeoDataFrame.
+
+    Parameters
+    ----------
+    query : string or dict
+        query string or structured dict to geocode/download
+    which_result : int
+        max number of results to return and which to process upon receipt
+
+    Returns
+    -------
+    gdf : geopandas.GeoDataFrame
+    """
     data = downloader._osm_polygon_download(query, limit=which_result)
-    if len(data) >= which_result:
 
+    if len(data) >= which_result:
         # extract data elements from the JSON response
         result = data[which_result - 1]
         bbox_south, bbox_north, bbox_west, bbox_east = [float(x) for x in result["boundingbox"]]
@@ -97,71 +152,10 @@ def gdf_from_place(query, which_result=1, buffer_dist=None):
         if geometry["type"] not in ["Polygon", "MultiPolygon"]:
             utils.log(f'OSM returned a {geometry["type"]} as the geometry', level=lg.WARNING)
 
-        # create the GeoDataFrame, name it, and set its original CRS to default_crs
-        gdf = gpd.GeoDataFrame.from_features(features)
-        gdf.crs = settings.default_crs
-
-        # if buffer_dist was passed in, project the geometry to UTM, buffer it
-        # in meters, then project it back to lat-lng
-        if buffer_dist is not None:
-            gdf_utm = projection.project_gdf(gdf)
-            gdf_utm["geometry"] = gdf_utm["geometry"].buffer(buffer_dist)
-            gdf = projection.project_gdf(gdf_utm, to_latlong=True)
-            utils.log(f"Buffered GeoDataFrame to {buffer_dist} meters")
-
-        # return the gdf
-        utils.log(f'Created GeoDataFrame with {len(gdf)} row for query "{query}"')
-        return gdf
+        # create the GeoDataFrame
+        return gpd.GeoDataFrame.from_features(features)
     else:
         # if no data returned (or fewer results than which_result)
-        utils.log(
-            f'OSM returned no results (or fewer than which_result) for query "{query}"',
-            level=lg.WARNING,
-        )
+        msg = f'OSM returned no results (or fewer than which_result) for query "{query}"'
+        utils.log(msg, level=lg.WARNING)
         return gpd.GeoDataFrame()
-
-
-def gdf_from_places(queries, which_results=None, buffer_dist=None):
-    """
-    Create a GeoDataFrame from a list of place name queries.
-
-    Geocode the queries with Nominatim then turn result into GeoDataFrame with
-    a geometry column.
-
-    Parameters
-    ----------
-    queries : list
-        list of query strings or structured query dicts to geocode/download,
-        one at a time
-    which_results : list
-        if not None, a list of max number of results to return and which to
-        process upon receipt, for each query in queries
-    buffer_dist : float
-        distance to buffer around the place geometry, in meters
-
-    Returns
-    -------
-    gdf : geopandas.GeoDataFrame
-    """
-    # create an empty GeoDataFrame then append each result as a new row,
-    # checking for the presence of which_results
-    gdf = gpd.GeoDataFrame()
-    if which_results is not None:
-
-        if len(queries) != len(which_results):
-            raise ValueError("which_results length must equal queries length")
-
-        for query, which_result in zip(queries, which_results):
-            gdf_tmp = gdf_from_place(query, buffer_dist=buffer_dist, which_result=which_result)
-            gdf = gdf.append(gdf_tmp)
-    else:
-        for query in queries:
-            gdf = gdf.append(gdf_from_place(query, buffer_dist=buffer_dist))
-
-    # reset the index
-    gdf = gdf.reset_index(drop=True)
-
-    # set the original CRS of the GeoDataFrame to default_crs, and return it
-    gdf.crs = settings.default_crs
-    utils.log(f"Finished creating GeoDataFrame with {len(gdf)} rows from {len(queries)} queries")
-    return gdf
