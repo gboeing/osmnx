5fecb78dc3666b47f992d9748f71cc64eb5f089f
Geoff Boeing <boeing@usc.edu>
2020-07-03 11:40:29 -0700

    version bump

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ else:
 # now call setup
 setup(
     name="osmnx",
-    version="0.15.0",
+    version="0.15.1",
     description=DESC,
     long_description=LONG_DESCRIPTION,
     classifiers=CLASSIFIERS,
