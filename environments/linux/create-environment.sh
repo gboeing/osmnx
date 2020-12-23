c849c21e40df1e83ec7d0f17c40a989f6eaadb98
Geoff Boeing <boeing@usc.edu>
2020-12-22 16:47:12 -0800

    use mamba

--- environments/linux/create-environment.sh
+++ environments/linux/create-environment.sh
@@ -2,18 +2,17 @@
 eval "$(conda shell.bash hook)"
 conda deactivate
 conda env remove -n ox --yes
-conda clean --all --yes
+conda clean --all --yes --quiet
 conda config --set show_channel_urls true
 conda config --set channel_priority strict
 conda config --prepend channels conda-forge
-conda update conda -n base --yes
-conda create -n ox --file "../docker/requirements.txt" python-igraph --yes
+mamba create -n ox --file "../docker/requirements.txt" python-igraph --yes
 eval "$(conda shell.bash hook)"
 conda activate ox
 pip uninstall osmnx --yes
 pip install -e ../../.
 python -m ipykernel install --sys-prefix --name ox --display-name "Python (ox)"
-conda clean --all --yes
+conda clean --all --yes --quiet
 conda env export -n ox > environment.yml
 conda list
 jupyter kernelspec list
