78163be19fa3648b29c8f4d50117fdafae99b695
Geoff Boeing <boeing@usc.edu>
2021-01-21 12:07:32 -0800

    move python-igraph to requirements file

--- environments/linux/create-environment.sh
+++ environments/linux/create-environment.sh
@@ -7,7 +7,7 @@ conda clean --all --yes --quiet
 conda config --set show_channel_urls true
 conda config --set channel_priority strict
 conda config --prepend channels conda-forge
-mamba create -n ox --file "../docker/requirements.txt" python-igraph --yes
+mamba create -n ox --file "../docker/requirements.txt" --yes
 eval "$(conda shell.bash hook)"
 conda activate ox
 pip uninstall osmnx --yes
--- environments/windows/create-environment.bat
+++ environments/windows/create-environment.bat
@@ -8,7 +8,6 @@ CALL mamba create -n ox --file "../docker/requirements.txt" --yes
 CALL conda activate ox
 CALL pip uninstall osmnx --yes
 CALL pip install -e ../../.
-CALL pip install https://ocf.berkeley.edu/~gboeing/share/python_igraph-0.8.3-cp39-cp39-win_amd64.whl
 CALL python -m ipykernel install --sys-prefix --name ox --display-name "Python (ox)"
 CALL conda clean --all --yes --quiet
 CALL conda env export > environment.yml
