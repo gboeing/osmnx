f4569081902017b3118ba4c345f9269cccd3cd9c
Geoff Boeing <boeing@usc.edu>
2022-08-22 01:34:19 -0700

    update env script

--- environments/windows/create-environment.bat
+++ environments/windows/create-environment.bat
@@ -1,14 +1,16 @@
-CALL %USERPROFILE%\mambaforge\Scripts\activate.bat
-CALL mamba deactivate
-CALL mamba env remove -n ox --yes
-CALL mamba clean --all --yes --quiet
-CALL mamba create -c conda-forge --strict-channel-priority -n ox --file "../docker/requirements.txt" --yes
-CALL conda activate ox
-CALL pip uninstall osmnx --yes
-CALL pip install -e ../../.
-CALL python -m ipykernel install --sys-prefix --name ox --display-name "Python (ox)"
-CALL mamba clean --all --yes --quiet
-CALL mamba env export > environment.yml
-CALL mamba list
-CALL jupyter kernelspec list
-CALL ipython -c "import osmnx; print('OSMnx version', osmnx.__version__)"
+SET ENV="ox"
+SET PACKAGE="osmnx"
+CALL %USERPROFILE%\mambaforge\Scripts\activate.bat && ^
+conda activate base && ^
+mamba env remove -n %ENV% --yes && ^
+mamba clean --all --yes --quiet --no-banner && ^
+mamba create -c conda-forge --strict-channel-priority -n %ENV% --file "../docker/requirements.txt" --yes --no-banner && ^
+conda activate %ENV% && ^
+pip uninstall %PACKAGE% --yes && ^
+pip install -e ../../. && ^
+python -m ipykernel install --sys-prefix --name %ENV% --display-name "Python (%ENV%)" && ^
+mamba clean --all --yes --quiet --no-banner && ^
+mamba env export -n %ENV% > environment.yml && ^
+mamba list --no-banner && ^
+jupyter kernelspec list && ^
+ipython -c "import %PACKAGE%; print('%PACKAGE% version', %PACKAGE%.__version__)"
