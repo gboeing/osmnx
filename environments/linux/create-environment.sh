c853ec305f4fee3fa3d165e38dd961c7973f2c89
Geoff Boeing <boeing@usc.edu>
2022-08-22 01:34:00 -0700

    no banner

--- environments/linux/create-environment.sh
+++ environments/linux/create-environment.sh
@@ -3,17 +3,17 @@ set -e
 ENV=ox
 PACKAGE=osmnx
 eval "$(conda shell.bash hook)"
-conda deactivate
+conda activate base
 mamba env remove -n $ENV --yes
-mamba clean --all --yes --quiet
-mamba create -c conda-forge --strict-channel-priority -n $ENV --file "../docker/requirements.txt" --yes
+mamba clean --all --yes --quiet --no-banner
+mamba create -c conda-forge --strict-channel-priority -n $ENV --file "../docker/requirements.txt" --yes --no-banner
 eval "$(conda shell.bash hook)"
 conda activate $ENV
 pip uninstall $PACKAGE --yes
 pip install -e ../../.
 python -m ipykernel install --sys-prefix --name $ENV --display-name "Python ($ENV)"
-mamba clean --all --yes --quiet
+mamba clean --all --yes --quiet --no-banner
 mamba env export -n $ENV > environment.yml
-mamba list
+mamba list --no-banner
 jupyter kernelspec list
 ipython -c "import $PACKAGE; print('$PACKAGE version', $PACKAGE.__version__)"
