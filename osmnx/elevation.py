be3690f3b34c92490ad784aa31b7737d5305e816
Nick ODell <nickodell@gmail.com>
2022-08-30 11:30:55 -0600

    Revert "Convert numpy array to tuples before passing to raster.sample()"
    Fixes #863.
    
    This commit was originally added to fix a rasterio bug which has now
    been resolved upstream. See rasterio/rasterio#2547 for details of bug,
    and PR #842 for how it applies to OSMnx.
    
    This reverts commit 4ba2b1dbb3e15ab12f7eb5a807e481844948542f.

--- osmnx/elevation.py
+++ osmnx/elevation.py
@@ -42,8 +42,7 @@ def _query_raster(nodes, filepath, band):
     """
     # must open raster file here: cannot pickle it to pass in multiprocessing
     with rasterio.open(filepath) as raster:
-        nodes_generator = (tuple(x) for x in nodes.values)
-        values = np.array(tuple(raster.sample(nodes_generator, band)), dtype=float).squeeze()
+        values = np.array(tuple(raster.sample(nodes.values, band)), dtype=float).squeeze()
         values[values == raster.nodata] = np.nan
         return zip(nodes.index, values)
 
