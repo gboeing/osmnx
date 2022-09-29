cff61adcec628cbc3e1ecb05c38de3d460eba5c2
Geoff Boeing <boeing@usc.edu>
2022-09-29 08:53:06 -0700

    optionally tag latest

--- environments/docker/docker-build.sh
+++ environments/docker/docker-build.sh
@@ -4,8 +4,20 @@ PACKAGE=osmnx
 
 # get the version number for the tag
 read -r -p "What is the $PACKAGE version number? " VERSION
-TAG=v$VERSION
-echo "Image will be tagged $DOCKERUSER/$PACKAGE:$TAG and $DOCKERUSER/$PACKAGE:latest"
+TAG="$DOCKERUSER/$PACKAGE:v$VERSION"
+ALLTAGS="$TAG"
+TAGARGS="-t $TAG"
+
+# optionally tag image with "latest" as well
+read -r -n 1 -p "Also tag this image \"latest\"? (y/n) " INPUT
+echo ""
+if [[ $INPUT == "Y" || $INPUT == "y" ]]; then
+    ALLTAGS="$ALLTAGS $DOCKERUSER/$PACKAGE:latest"
+    TAGARGS="$TAGARGS -t $DOCKERUSER/$PACKAGE:latest"
+fi
+
+# confirm then proceed
+echo "Image will be tagged \"$ALLTAGS\""
 read -r -n 1 -p "Ready to proceed? (y/n) " INPUT
 echo ""
 if [[ $INPUT != "Y" && $INPUT != "y" ]]; then
@@ -20,12 +32,12 @@ docker rmi $(docker images -q) --force
 # login then multi-platform buildx the image and push it to the hub
 set -e
 docker login
-docker buildx build --push --platform=linux/amd64,linux/arm64 -t $DOCKERUSER/$PACKAGE:$TAG -t $DOCKERUSER/$PACKAGE:latest .
+docker buildx build --push --platform=linux/amd64,linux/arm64 $TAGARGS .
 
 # import package and print its version as a test, then export conda env to yml
-echo "Pushed $DOCKERUSER/$PACKAGE:$TAG to Docker Hub"
+echo "Pushed $ALLTAGS to Docker Hub"
 echo "Testing image..."
-IMPORTED_VERSION=$(docker run --rm $DOCKERUSER/$PACKAGE:$TAG /bin/bash -c "python -c \"import $PACKAGE; print($PACKAGE.__version__)\"")
+IMPORTED_VERSION=$(docker run --rm $TAG /bin/bash -c "python -c \"import $PACKAGE; print($PACKAGE.__version__)\"")
 echo "Imported $PACKAGE version $IMPORTED_VERSION, expected $VERSION"
-docker run --rm -v "$PWD":/home/jovyan/work $DOCKERUSER/$PACKAGE:$TAG /bin/bash -c "conda env export -n base > /home/jovyan/work/environment.yml"
-echo "Exported conda env to yml file"
+docker run --rm -v "$PWD":/home/jovyan/work $TAG /bin/bash -c "conda env export -n base > /home/jovyan/work/environment.yml"
+echo "Exported conda env to environment.yml"
