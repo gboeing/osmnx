d8b9baaedd8ffb52b2e733e3c25ab21318591e92
Geoff Boeing <boeing@usc.edu>
2023-06-02 21:28:17 -0700

    alpha versioning

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -57,7 +57,7 @@ author = "Geoff Boeing"
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
 # built documents.
-version = release = "1.3.1"
+version = release = "1.4.0-alpha"
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #
