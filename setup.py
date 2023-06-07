cf1b23483448d230b9cc1f4d7f229f1d8880b2f8
Geoff Boeing <gboeing@users.noreply.github.com>
2023-06-07 11:39:38 -0700

    classifier for python 3 only

--- setup.py
+++ setup.py
@@ -38,6 +38,7 @@ CLASSIFIERS = [
     "Operating System :: OS Independent",
     "Programming Language :: Python",
     "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3 :: Only",
     "Programming Language :: Python :: 3.9",
     "Programming Language :: Python :: 3.10",
     "Programming Language :: Python :: 3.11",
