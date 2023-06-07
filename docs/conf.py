b6de5db32b5054234a11c665649d84a227205bc2
Geoff Boeing <boeing@usc.edu>
2023-06-06 19:49:51 -0700

    rtd theme

--- docs/conf.py
+++ docs/conf.py
@@ -46,5 +46,5 @@ language = "en"
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-html_theme = "default"
+html_theme = "sphinx_rtd_theme"
 html_static_path = []
