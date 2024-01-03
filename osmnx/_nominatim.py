3e77e2750454ae572717f5f8b3ea51fe4f401ce9
Geoff Boeing <boeing@usc.edu>
2024-01-02 20:52:09 -0800

    add type hints to _nominatim module

--- osmnx/_nominatim.py
+++ osmnx/_nominatim.py
@@ -1,8 +1,11 @@
 """Tools to work with the Nominatim API."""
 
+from __future__ import annotations
+
 import logging as lg
 import time
 from collections import OrderedDict
+from typing import Any
 
 import requests
 
@@ -11,7 +14,9 @@ from . import settings
 from . import utils
 
 
-def _download_nominatim_element(query, by_osmid=False, limit=1, polygon_geojson=1):
+def _download_nominatim_element(
+    query: str | dict, by_osmid: bool = False, limit: int = 1, polygon_geojson: bool = True
+) -> dict:
     """
     Retrieve an OSM element from the Nominatim API.
 
@@ -24,7 +29,7 @@ def _download_nominatim_element(query, by_osmid=False, limit=1, polygon_geojson=
     limit : int
         max number of results to return
     polygon_geojson : int
-        retrieve the place's geometry from the API, 0=no, 1=yes
+        whether to retrieve the place's geometry from the API
 
     Returns
     -------
@@ -32,9 +37,9 @@ def _download_nominatim_element(query, by_osmid=False, limit=1, polygon_geojson=
         JSON response from the Nominatim server
     """
     # define the parameters
-    params = OrderedDict()
+    params: OrderedDict[Any] = OrderedDict()
     params["format"] = "json"
-    params["polygon_geojson"] = polygon_geojson
+    params["polygon_geojson"] = int(polygon_geojson)
 
     if by_osmid:
         # if querying by OSM ID, use the lookup endpoint
@@ -64,7 +69,9 @@ def _download_nominatim_element(query, by_osmid=False, limit=1, polygon_geojson=
     return _nominatim_request(params=params, request_type=request_type)
 
 
-def _nominatim_request(params, request_type="search", pause=1, error_pause=60):
+def _nominatim_request(
+    params: OrderedDict, request_type: str = "search", pause: float = 1, error_pause: float = 60
+) -> dict:
     """
     Send a HTTP GET request to the Nominatim API and return response.
 
@@ -91,9 +98,9 @@ def _nominatim_request(params, request_type="search", pause=1, error_pause=60):
     # prepare Nominatim API URL and see if request already exists in cache
     url = settings.nominatim_endpoint.rstrip("/") + "/" + request_type
     params["key"] = settings.nominatim_key
-    prepared_url = requests.Request("GET", url, params=params).prepare().url
-    cached_response_json = _downloader._retrieve_from_cache(prepared_url)
-    if cached_response_json is not None:
+    prepared_url = str(requests.Request("GET", url, params=params).prepare().url)
+
+    if (cached_response_json := _downloader._retrieve_from_cache(prepared_url)) is not None:
         return cached_response_json
 
     # pause then request this URL
@@ -122,5 +129,5 @@ def _nominatim_request(params, request_type="search", pause=1, error_pause=60):
         return _nominatim_request(params, request_type, pause, error_pause)
 
     response_json = _downloader._parse_response(response)
-    _downloader._save_to_cache(prepared_url, response_json, response.status_code)
+    _downloader._save_to_cache(prepared_url, response_json, response.ok)
     return response_json
