ace5f7d3d4b81947b7cb4934ceb6c6bef3ab4f96
Geoff Boeing <boeing@usc.edu>
2024-02-15 10:36:40 -0800

    misc improvements to ci and tests

--- tests/lint_test.sh
+++ tests/lint_test.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-
-# exit on error
-set -e
+set -e  # exit on error
 
 # delete temp files and folders
 rm -r -f .pytest_cache .temp ./dist/ ./docs/build osmnx/__pycache__ tests/__pycache__
@@ -9,19 +7,13 @@ find . -type f -name "*.vrt" -delete
 find . -type f -name ".coverage*" -delete
 
 # lint
-#ruff check . --preview
 pre-commit run --all-files
 
-# test building and validating the package
-hatch build --clean
-twine check --strict ./dist/*
-
 # build the docs
-make -C ./docs html SPHINXOPTS="-W --keep-going"
-#python -m sphinx -b linkcheck ./docs/source ./docs/build/linkcheck
+make -C ./docs html SPHINXOPTS="-E -W --keep-going"
 
 # run the tests and report the test coverage
-pytest --maxfail=1 --typeguard-packages=osmnx --cov=./osmnx --cov-report=term-missing --verbose
+pytest --verbose --maxfail=1 --typeguard-packages=osmnx --cov=osmnx --cov-report=term-missing:skip-covered
 
 # delete temp files and folders
 rm -r -f .pytest_cache .temp ./dist/ ./docs/build osmnx/__pycache__ tests/__pycache__
