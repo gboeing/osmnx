0ac772c350ef1aa5c0faffa0bf4e3acad8da0bbe
Geoff Boeing <boeing@usc.edu>
2024-05-06 14:37:08 -0700

    update ci

--- environments/linux/create-environment.sh
+++ environments/linux/create-environment.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 set -e
 ENV=ox
+ENV_PATH=$(conda info --base)/envs/$ENV
 PACKAGE=osmnx
 eval "$(conda shell.bash hook)"
 conda activate base
-mamba env remove -n $ENV --yes
-mamba clean --all --yes --quiet --no-banner
-mamba create -c conda-forge --strict-channel-priority -n $ENV --file "../docker/requirements.txt" --yes --no-banner
+conda env remove -n $ENV --yes
+mamba create -n $ENV --yes -c conda-forge --strict-channel-priority --file "../docker/requirements.txt"
 eval "$(conda shell.bash hook)"
 conda activate $ENV
-uv pip uninstall $PACKAGE
-uv pip install -e ../../.
-python -m ipykernel install --sys-prefix --name $ENV --display-name "Python ($ENV)"
-mamba clean --all --yes --quiet --no-banner
-mamba env export -n $ENV > environment.yml
-mamba list --no-banner
+python -m pip --python $ENV_PATH uninstall $PACKAGE --yes
+python -m pip --python $ENV_PATH install -e ../../.
+python -m ipykernel install --prefix $ENV_PATH --name $ENV --display-name "Python ($ENV)"
+conda env export -n $ENV > environment.yml
+conda list -n $ENV
 jupyter kernelspec list
 ipython -c "import $PACKAGE; print('$PACKAGE version', $PACKAGE.__version__)"
--- environments/windows/create-environment.bat
+++ environments/windows/create-environment.bat
@@ -1,16 +1,16 @@
-SET ENV="ox"
-SET PACKAGE="osmnx"
-CALL %USERPROFILE%\miniforge3\Scripts\activate.bat && ^
-conda deactivate && ^
-mamba env remove -n %ENV% --yes && ^
-mamba clean --all --yes --quiet --no-banner && ^
-mamba create -c conda-forge --strict-channel-priority -n %ENV% --file "../docker/requirements.txt" --yes --no-banner && ^
+SET CONDA_ROOT=%USERPROFILE%\miniforge3
+SET ENV=ox
+SET ENV_PATH=%CONDA_ROOT%\envs\%ENV%
+SET PACKAGE=osmnx
+CALL %CONDA_ROOT%\Scripts\activate.bat && ^
+conda activate base && ^
+conda env remove -n %ENV% --yes && ^
+mamba create -n %ENV% --yes -c conda-forge --strict-channel-priority --file "../docker/requirements.txt" && ^
 conda activate %ENV% && ^
-uv pip uninstall %PACKAGE% && ^
-uv pip install -e ../../. && ^
-python -m ipykernel install --sys-prefix --name %ENV% --display-name "Python (%ENV%)" && ^
-mamba clean --all --yes --quiet --no-banner && ^
-mamba env export -n %ENV% > environment.yml && ^
-mamba list --no-banner && ^
+python -m pip --python %ENV_PATH%\python.exe uninstall %PACKAGE% --yes && ^
+python -m pip --python %ENV_PATH%\python.exe install -e ../../. && ^
+python -m ipykernel install --prefix %ENV_PATH% --name %ENV% --display-name "Python (%ENV%)" && ^
+conda env export -n %ENV% > environment.yml && ^
+conda list && ^
 jupyter kernelspec list && ^
 ipython -c "import %PACKAGE%; print('%PACKAGE% version', %PACKAGE%.__version__)"
