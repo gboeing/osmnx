e2c621aee8617e18b65abdb8948059dfbea3495b
Geoff Boeing <boeing@usc.edu>
2024-12-08 10:56:07 -0800

    update workflows

--- environments/docker/docker-build.sh
+++ environments/docker/docker-build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
+#docker image prune -af && docker system prune -af && docker volume prune -af && docker system df
 docker login
-docker buildx build --no-cache --pull --push --platform=linx/amd64,linux/arm64 -f ./environments/docker/Dockerfile -t gboeing/osmnx:test .
+docker buildx build --no-cache --pull --push --platform=linux/amd64,linux/arm64 -f ./environments/docker/Dockerfile -t gboeing/osmnx:test .
 IMPORTED_VERSION=$(docker run --rm gboeing/osmnx:test /bin/bash -c "ipython -c \"import osmnx; print(osmnx.__version__)\"")
 echo "Imported $IMPORTED_VERSION"
