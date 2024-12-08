682191ecd790ee1bd3aeb3bdd63f753881a0de3d
Geoff Boeing <boeing@usc.edu>
2024-12-07 21:31:50 -0800

    add docker publish workflow

--- environments/docker/docker-build.sh
+++ environments/docker/docker-build.sh
@@ -1,43 +1,6 @@
 #!/bin/bash
-DOCKERUSER=gboeing
-PACKAGE=osmnx
-
-# get the version number for the tag
-read -r -p "What is the $PACKAGE version number? " VERSION
-TAG="$DOCKERUSER/$PACKAGE:v$VERSION"
-ALLTAGS="$TAG"
-TAGARGS="-t $TAG"
-
-# optionally tag image with "latest" as well
-read -r -n 1 -p "Also tag this image \"latest\"? (y/n) " INPUT
-echo ""
-if [[ $INPUT == "Y" || $INPUT == "y" ]]; then
-    ALLTAGS="$ALLTAGS $DOCKERUSER/$PACKAGE:latest"
-    TAGARGS="$TAGARGS -t $DOCKERUSER/$PACKAGE:latest"
-fi
-
-# confirm then proceed
-echo "Image will be tagged \"$ALLTAGS\""
-read -r -n 1 -p "Ready to proceed? (y/n) " INPUT
-echo ""
-if [[ $INPUT != "Y" && $INPUT != "y" ]]; then
-    exit 1
-fi
-
-# remove any existing containers or images
-docker stop $(docker ps -aq)
-docker rm $(docker ps -aq)
-docker rmi $(docker images -q) --force
-
-# login then multi-platform buildx the image and push it to the hub
 set -e
 docker login
-docker buildx build --no-cache --push --platform=linux/amd64,linux/arm64 $TAGARGS .
-
-# import package and print its version as a test, then export conda env to yml
-echo "Pushed $ALLTAGS to Docker Hub"
-echo "Testing image..."
-IMPORTED_VERSION=$(docker run --rm $TAG /bin/bash -c "ipython -c \"import $PACKAGE; print($PACKAGE.__version__)\"")
-echo "Imported $PACKAGE version $IMPORTED_VERSION, expected $VERSION"
-docker run --rm -v "$PWD":/home/jovyan/work $TAG /bin/bash -c "conda env export -n base > /home/jovyan/work/environment.yml"
-echo "Exported conda env to environment.yml"
+docker buildx build --no-cache --pull --load --platform=linux/arm64 -f environments/docker-test/Dockerfile -t gboeing/osmnx:test .
+IMPORTED_VERSION=$(docker run --rm gboeing/osmnx:test /bin/bash -c "ipython -c \"import osmnx; print(osmnx.__version__)\"")
+echo "Imported $IMPORTED_VERSION"
