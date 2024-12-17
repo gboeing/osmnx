8e0e0ae801abf9573d56eb9362afa1e2ddb21ffe
Geoff Boeing <boeing@usc.edu>
2024-12-16 17:32:13 -0800

    add links to user reference modules in getting started guide

--- osmnx/convert.py
+++ osmnx/convert.py
@@ -381,7 +381,7 @@ def to_undirected(G: nx.MultiDiGraph) -> nx.MultiGraph:
     Convert MultiDiGraph to undirected MultiGraph.
 
     This function has a limited use case: it allows you to create a MultiGraph
-    for use with functions/algorithms which only accept a MultiGraph object.
+    for use with functions/algorithms that only accept a MultiGraph object.
     Rather, if you want a fully bidirectional graph (such as for a walking
     network), configure the `settings` module's `bidirectional_network_types`
     before creating your graph to generate a fully bidirectional MultiDiGraph.
