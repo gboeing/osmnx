a327e329aace12e8b9db12a6f065a9c868b4610f
Geoff Boeing <boeing@usc.edu>
2025-05-12 11:31:14 -0700

    ignore doi.org in linkcheck because some of its redirects get 403

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -45,22 +45,8 @@ autodoc_mock_imports = [
     "sklearn",
 ]
 
-# set necessary linkcheck headers to avoid 403 from DOI redirect
-headers = {
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-    "Accept-Encoding": "gzip, deflate, br, zstd",
-    "Accept-Language": "en-US,en;q=0.5",
-    "Priority": "u=0, i",
-    "Referer": "https://duckduckgo.com/",
-    "Sec-Fetch-Dest": "document",
-    "Sec-Fetch-Mode": "navigate",
-    "Sec-Fetch-Site": "cross-site",
-    "Sec-Fetch-User": "?1",
-    "TE": "trailers",
-    "Upgrade-Insecure-Requests": "1",
-    "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:138.0) Gecko/20100101 Firefox/138.0",
-}
-linkcheck_request_headers = {"*": headers}
+# linkcheck for some DOI redirects gets HTTP 403 in CI environment
+linkcheck_ignore = [r"https://doi\.org/.*"]
 
 # type annotations configuration
 autodoc_typehints = "description"
