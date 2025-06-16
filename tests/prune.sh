7ba5d357ba273b4c951e8c8624f741a541dcb0c5
Geoff Boeing <boeing@usc.edu>
2025-06-16 11:19:20 -0700

    update prune script

--- tests/prune.sh
+++ tests/prune.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
-# prune conda, docker, and git
 set -euo pipefail
+
+# clean the caches of pip and uv
+pip cache purge
+uv cache clean
+
+# deactivate conda and clean its cache
 eval "$(conda shell.bash hook)"
-conda activate base
+conda deactivate
 conda clean --all --yes
+
+# remove unused docker images, data, and local volumes
 docker image prune -af && docker system prune -af && docker volume prune -af
+
+# prune, repack, and garbage collect git
 git remote prune origin
 git repack -a -d -f --depth=250 --window=250
 git prune-packed
