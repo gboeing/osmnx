c4d467f5ccd52138c619f8a5a2125e177f7bf654
Geoff Boeing <boeing@usc.edu>
2025-06-17 13:48:25 -0700

    update docker build

--- environments/docker/install.sh
+++ environments/docker/install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -euo pipefail
 
-# rasterio doesn't have linux/arm64 wheels, so if this platform target is
-# linux/arm64 then don't install this optional dependency (attempting to
-# build it rather than install the wheel also fails).
+# rasterio doesn't provide linux/arm64 wheels, so if the target platform is
+# linux/arm64, don't install this optional dependency (attempting to build
+# it rather than install the wheel will also fail).
 if [[ "$TARGETPLATFORM" == "linux/arm64" ]]
 then
   NOEXTRA="--no-extra=raster"
@@ -11,9 +11,15 @@ else
   NOEXTRA=""
 fi
 
+# install uv and add the executable to the PATH
 wget -qO- https://astral.sh/uv/0.7.12/install.sh | sh
 source $HOME/.local/bin/env
-uv sync --no-cache --no-build --system --no-dev --all-extras
+
+# add extra packages necessary for running examples' notebooks
+# then install everything into the existing system environment
+uv add --no-sync --optional=examples folium jupyterlab mapclassify python-igraph
+uv export --no-cache --no-build --no-dev --all-extras $NOEXTRA > requirements.txt
+uv pip install --no-cache --no-build --system --strict -r requirements.txt
 uv cache clean
 uv pip check
 uv pip list
