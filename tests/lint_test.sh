a8307ced57d76165775b8dc8a95408225fcdc58a
Geoff Boeing <boeing@usc.edu>
2025-06-19 09:37:24 -0700

    activate venv instead of uv run each command

--- tests/lint_test.sh
+++ tests/lint_test.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -euo pipefail
 
-# sync the virtual environment
-uv sync --all-extras --dev
+# activate the virtual environment
+source .venv/bin/activate
 
 # delete temp files and folders
 rm -r -f ./.coverage* ./.pytest_cache ./.temp ./dist ./docs/build ./*/__pycache__
 
 # run the pre-commit hooks for linting/formatting
-SKIP=no-commit-to-branch uv run pre-commit run --all-files
+SKIP=no-commit-to-branch pre-commit run --all-files
 
 # build and validate the package
 uv build
@@ -16,11 +16,11 @@ uvx twine check --strict ./dist/*
 uvx --from=validate-pyproject[all] validate-pyproject ./pyproject.toml
 
 # run the tests and report the test coverage
-uv run pytest --verbose --maxfail=1 --typeguard-packages=osmnx --cov=osmnx --cov-report=term-missing:skip-covered --numprocesses=3 --dist=loadgroup
+pytest --verbose --maxfail=1 --typeguard-packages=osmnx --cov=osmnx --cov-report=term-missing:skip-covered --numprocesses=3 --dist=loadgroup
 
 # build the docs and test that links are alive
-uv run sphinx-build -q -a -E -W --keep-going -b html ./docs/source ./docs/build/html
-uv run sphinx-build -q -a -E -W --keep-going -b linkcheck ./docs/source ./docs/build/linkcheck
+sphinx-build -q -a -E -W --keep-going -b html ./docs/source ./docs/build/html
+sphinx-build -q -a -E -W --keep-going -b linkcheck ./docs/source ./docs/build/linkcheck
 
 # delete temp files and folders
 rm -r -f ./.coverage* ./.pytest_cache ./.temp ./dist ./docs/build ./*/__pycache__
