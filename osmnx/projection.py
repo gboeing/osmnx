20a9cfa89769d4f42597a1e69885198798fc857b
bridgesign <eyeoeternity@gmail.com>
2026-03-24 04:39:07 +0000

    No reversion just check projection

--- osmnx/projection.py
+++ osmnx/projection.py
@@ -117,7 +117,20 @@ def project_gdf(
 
     # else if to_crs is None, project gdf to an appropriate UTM zone
     elif to_crs is None:
-        to_crs = gdf.estimate_utm_crs()
+        # if polygon is outside UTM limits (80 deg south, 84 deg north), then
+        # we must use universal polar stereographic coordinate system instead
+        # however, skip this check if the gdf is already projected, since it may be in a
+        # projected CRS that is outside UTM limits but still valid for the geometry
+        unprojected = not is_projected(gdf.crs)
+        UTM_SOUTH_LIMIT = -80
+        UTM_NORTH_LIMIT = 84
+        if unprojected and gdf.total_bounds[1] < UTM_SOUTH_LIMIT:
+            to_crs = "epsg:32761"
+        elif unprojected and gdf.total_bounds[3] > UTM_NORTH_LIMIT:
+            to_crs = "epsg:32661"
+        else:
+            # otherwise, we're within UTM limits, so determine UTM zone
+            to_crs = gdf.estimate_utm_crs()
 
     # project the gdf
     gdf_proj = gdf.to_crs(to_crs)
